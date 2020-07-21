4ef48d3c8a88e3ed53d7f7e09668c04e30a7b1a9
Adam Gerecke <adamgerecke@quickenloans.com>
2020-07-21 15:12:08 -0400

    Added more checks for a word that starts with 3 consonants.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Lab_4.Bonus_Igpay_Atinlay
 {
     class Program
     {
-        static void ConsonantVowel (string word)
+        static void ConsonantVowel(string word)
         {
             List<char> wordList = new List<char>();
 
@@ -15,20 +15,20 @@ namespace Lab_4.Bonus_Igpay_Atinlay
             char firstLetter = wordList[0];
 
             wordList.RemoveAt(0);
-            wordList.Insert(wordLength -1, firstLetter);
+            wordList.Insert(wordLength - 1, firstLetter);
 
-            string newWord = string.Join<char>("",wordList);
+            string newWord = string.Join<char>("", wordList);
 
-            Console.WriteLine(newWord+"ay");
-            
+            Console.WriteLine(newWord + "ay");
+
         }
 
-        static void Vowel (string word)
+        static void Vowel(string word)
         {
             Console.WriteLine(word + "way");
         }
 
-        static void TwoConsonants (string word)
+        static void TwoConsonants(string word)
         {
             List<char> wordList = new List<char>();
 
@@ -46,35 +46,68 @@ namespace Lab_4.Bonus_Igpay_Atinlay
 
             Console.WriteLine(newWord + "ay");
         }
-        
-        
-        
+
+
         static void Main(string[] args)
         {
+            
+            bool keepTranslating = true;
+
             Console.WriteLine("Welcome to the Pig Latin Translator!");
             Console.WriteLine();
-            Console.Write("Enter a word to be translated:");
 
-            string word = Console.ReadLine().ToLower();
-            char[] wordArray = word.ToCharArray();
+            while ( keepTranslating)
+            {
+                Console.Write("Enter a word to be translated:");
+
+                string word = Console.ReadLine().ToLower();
+                char[] wordArray = word.ToCharArray();
+                List<char> wordList = new List<char>(wordArray);
+
+
+                List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
+                List<char> consonants = new List<char> { 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+
+
+
             
-            List<char> vowels = new List<char> { 'a', 'e', 'i', 'o', 'u' };
-            List<char> consonants = new List<char> { 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+            
 
-            if (consonants.Contains(wordArray[0]) && vowels.Contains(wordArray[1]))
-            {
-                ConsonantVowel(word); 
-            }
-            else if (vowels.Contains(wordArray[0]))
-            {
-                Vowel(word);
-            }
-            else if (consonants.Contains(wordArray[0]) && consonants.Contains(wordArray[1]))
-            {
-                TwoConsonants(word);
-            }
+                if (vowels.Contains(wordList[0])) // checks for a word that starts with a vowel ie. Apple
+                {
+                    Vowel(word);
+                }
+                else if (consonants.Contains(wordList[0]) && vowels.Contains(wordList[1])) // Checks for a Consonant followed by vowel ie. Banana
+                {
+                    ConsonantVowel(word);
+                }
+                else if (consonants.Contains(wordList[0]) && consonants.Contains(wordList[1]) && vowels.Contains(wordList[2])) // Checks if word starts with 2 consonants ie. Cherry
+                {
+                    TwoConsonants(word);
+                }
+                else if (consonants.Contains(wordList[0]) && consonants.Contains(wordList[1]) && consonants.Contains(wordList[2])) // Checks for a word that starts with 3 consonants ie. Gymnasium
+                {
+                    ConsonantVowel(word);
 
+                }
 
+                Console.WriteLine();
+                Console.Write("Translate another line? (y/n):");
+                string choice = Console.ReadLine().ToUpper();
+
+                if (choice == "N")
+                {
+                    keepTranslating = false;
+                    Console.WriteLine("Thankyou for using the translator.");
+                    Console.WriteLine("Press the Enter key to exit...");
+                    Console.ReadLine();
+                }
+                else if (choice != "Y")
+                {
+                    Console.WriteLine("That is not a Valid input. Translating another word.");
+                    continue;
+                }
+            }
         }
     }
 }
